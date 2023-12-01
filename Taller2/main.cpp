81ab262d7565b38605544912388e065d752e9d7b
Cokke <45271115+JorgeRandomGamer@users.noreply.github.com>
2023-12-01 12:42:18 -0300

    Actualizacion con comentarios

--- Taller2/main.cpp
+++ Taller2/main.cpp
@@ -11,6 +11,7 @@ int player = 1;
 class Connect4 {
     private:
     int tablero[filas][columnas];
+    int dificultad = 2;
 
     public:
     Connect4(){
@@ -19,6 +20,10 @@ class Connect4 {
                 tablero[i][j] = 0;
             }
         }
+    }
+
+    void setDificultad(int dif){
+        dificultad = dif;
     }
 
     void mostrarTablero(){
@@ -140,14 +145,17 @@ class Connect4 {
 
 
     int minimax(int jugadorActual, int profundidad, int alfa, int beta){
+        // si llega a la maxima profundidad o algun jugador gana, devuelve el puntaje
         if(profundidad == 0 || ganador(1) || ganador(2)) {
             return calcularPuntaje();
         }
-
+        
+        // inicializamos el mejor puntaje según el jugador actual.
         int mejorPuntaje;
         if(jugadorActual == 1){mejorPuntaje = INT_MIN;}
         else{mejorPuntaje = INT_MAX;}
 
+        // se revisa cada movimiento en el tablero y si la casilla esta vacia se realiza, luego se evalua el puntaje
         for(int col = 0; col < columnas; col++){
             for(int fila = filas - 1; fila >= 0; fila--){
                 if(tablero[fila][col] == 0){
@@ -165,6 +173,7 @@ class Connect4 {
 
                     tablero[fila][col] = 0;
 
+                    // Podamos el árbol si beta es menor o igual a alfa.
                     if (beta <= alfa) {
                         break;
                     }
@@ -184,7 +193,7 @@ class Connect4 {
                     if(tablero[fila][col] == 0){
                         tablero[fila][col] = 2;
                         
-                        int puntaje = minimax(1, 2, INT_MIN, INT_MAX);
+                        int puntaje = minimax(1, dificultad, INT_MIN, INT_MAX);
                         tablero[fila][col] = 0;
     
                         if(puntaje > mejorPuntaje){
@@ -207,20 +216,26 @@ class Connect4 {
 
 int main(){
     Connect4 juego;
+    int dificultad;
+    cout<<"\tConnect4\n";
+    cout<<"Seleccione dificultad: \n1)Facil\n2)Medio\n3)Dificil\n";cin>>dificultad;
+    juego.setDificultad(dificultad);
 
     for(int i = 0; i < filas * columnas; i++){
         
-        int jugadorActual = (i % 2 == 0) ? 1 : 2;
+        int jugadorActual;
+        if(i % 2 == 0){jugadorActual = 1;}
+        else{jugadorActual = 2;}
         
         cout<<"Turno del Jugador "<<jugadorActual<<endl;
         juego.mostrarTablero();
 
         if(jugadorActual == 1){
             int col;
-            cout<<"Ingrese la columna (1-7): ";cin>>col;
+            cout<<"Seleccione columna (1-7): ";cin>>col;
 
             while (!juego.hacerMovimiento(col-1, jugadorActual)) {
-                cout<<"Movimiento no válido. Ingrese la columna nuevamente: ";cin>>col;
+                cout<<"Movimiento invalido. Ingrese la columna nuevamente: ";cin>>col;
             }
         }else{juego.jugar();}
 
